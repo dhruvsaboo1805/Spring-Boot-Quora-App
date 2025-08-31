5bc329d6d9299709bc5105195c90e6b0f3c4a8bb
DhruvSaboo <saboodhruv789@gmail.com>
2025-08-31 16:05:09 +0530

    elastic search added

--- src/main/java/com/example/QuoraApp/repositories/IQuestionElasticSearchRepository.java
+++ src/main/java/com/example/QuoraApp/repositories/IQuestionElasticSearchRepository.java
@@ -2,7 +2,9 @@ package com.example.QuoraApp.repositories;
 
 import com.example.QuoraApp.models.QuestionElasticSearchDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
